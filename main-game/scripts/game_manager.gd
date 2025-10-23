954c8f568ccfc8b795b3f282ad3d5d6e78f5b4a9
Miguel Zschornack <120066884+zschornack@users.noreply.github.com>
2025-10-22 23:46:07 -0300

    Add files via upload

--- main-game/scripts/game_manager.gd
+++ main-game/scripts/game_manager.gd
@@ -2,8 +2,8 @@ extends Node
 
 var score = 0
 
-@onready var score_label = $/root/Game/UI/ScoreLabel
+@onready var score_label = $ScoreLabel
 
 func add_point():
 	score += 1
-	score_label.text = str(score) + " Catolica coins"
+	score_label.text = "You collected " + str(score) + " coins."
